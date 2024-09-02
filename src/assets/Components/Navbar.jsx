6986f45a768cbbf9da702e4e7a21e5d069983a92
Fahad Al-mubarak <fahadalmubarak1@outlook.com>
2024-09-02 20:18:13 +0100

    added navbar

---
+++ src/assets/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar () {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <a href="#">
+          <img src="" alt="logo" />
+        </a>
+      </div>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Skills</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
