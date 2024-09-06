626a30fcfaa49cbd6151e5a181f0b9106433313d
Fahad Al-mubarak <fahadalmubarak1@outlook.com>
2024-09-06 19:21:24 +0100

    added skills, projects, and connect functions

--- src/assets/Components/Banner.jsx
+++ src/assets/Components/Banner.jsx
@@ -5,7 +5,7 @@ import {useTypewriter, Cursor} from 'react-simple-typewriter';
 function Banner() {
 
     const [typeEffect] = useTypewriter({
-    words: ['Software Engineer', 'Web Developer'],
+    words: ['Software Engineer.', 'Full Stack Web Developer.'],
     loop: {},
     typeSpeed: 100,
     deleteSpeed: 40,
@@ -14,10 +14,14 @@ function Banner() {
 
 
   return (
-    <div className='container'>
+    <div className='container' id='Banner'>
       <div className='banner'>
         <h3>Welcome to My Portfolio</h3>
         <h1>My name is Fahad Al-Mubarak and I am a <span>{typeEffect}</span><span><Cursor/></span> </h1>
+          <div>
+            <a href="#Projects" className="btn">LinkedIn</a>
+            <a href="#Connect" className="btn">github</a>
+          </div>
       </div>
       <div className="image-div">
         <img src="/picture.jpg" alt="description" />
---
+++ src/assets/Components/Connect.jsx
@@ -0,0 +1,11 @@
+function Connect () {
+
+  <form action="">
+    <input type="text" placeholder="Name" />
+    <input type="email" placeholder="Email" />
+    <button>Submit</button>
+  </form>
+
+}
+
+export default Connect
