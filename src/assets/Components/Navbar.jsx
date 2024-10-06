5cb220a3e86c8169fad4a8b9a0771f7a103bd2c4
Fahad Al-mubarak <fahadalmubarak1@outlook.com>
2024-10-06 22:57:51 +0100

    changed skills position and translation switch from left to right

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -25,27 +25,27 @@ const Navbar = ({ onToggleLanguage, language }) => {
         </a>
       </div>
       <ul>
-          <div className="toggle-switch">
-            <input
-              type="checkbox"
-              id="language-toggle"
-              onChange={onToggleLanguage}
-              checked={language === 'ar'}
-            />
-            <label htmlFor="language-toggle" className="switch">
-              <span className="slider"></span>
-              <span className="labels">
-              <span className="label-en">EN</span>
-              <span className="label-ar">AR</span>
-              </span>
-            </label>
-          </div>
 
         <li><a href="#Banner">{translation[language].home}</a></li>
-        <li><a href="#Projects">{translation[language].projects}</a></li>
         <li><a href="#Skills">{translation[language].skills}</a></li>
+        <li><a href="#Projects">{translation[language].projects}</a></li>
         <li><a href="#Connect">{translation[language].connect}</a></li>
 
+        <div className="toggle-switch">
+          <input
+            type="checkbox"
+            id="language-toggle"
+            onChange={onToggleLanguage}
+            checked={language === 'ar'}
+          />
+          <label htmlFor="language-toggle" className="switch">
+            <span className="slider"></span>
+            <span className="labels">
+            <span className="label-en">EN</span>
+            <span className="label-ar">AR</span>
+            </span>
+          </label>
+        </div>
       </ul>
 
     </nav>
