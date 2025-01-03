64de79e47fdf38a7256e8dc5ae73f00b32b711f4
Fahad Al-mubarak <fahadalmubarak1@outlook.com>
2025-01-03 18:28:12 +0000

    added new skills

--- src/assets/Components/Skills.jsx
+++ src/assets/Components/Skills.jsx
@@ -7,13 +7,15 @@ const skills = [
   { name: 'HTML', logo: './Images/html5.svg' },
   { name: 'CSS', logo: './Images/css3.svg' },
   { name: 'JavaScript', logo: './Images/javascript.svg' },
-  { name: 'React', logo: './Images/react.svg' },
-  { name: 'Sass', logo: './Images/sass.svg' },
-  { name: 'Ruby', logo: './Images/ruby.svg' },
-  { name: 'Ruby on RaIls', logo: './Images/rubyonrails.svg' },
-  { name: 'Python', logo: './Images/python.svg' },
+  { name: 'C#', logo: './Images/csharp.svg' },
+  { name: '.Net', logo: './Images/dotnet.svg' },
   { name: 'postgreSQL', logo: './Images/postgresql.svg' },
   { name: 'Github', logo: './Images/github.svg' },
+  { name: 'Ruby', logo: './Images/ruby.svg' },
+  { name: 'Ruby on RaIls', logo: './Images/rubyonrails.svg' },
+  { name: 'React', logo: './Images/react.svg' },
+  { name: 'Sass', logo: './Images/sass.svg' },
+  { name: 'Python', logo: './Images/python.svg' },
 
   // Add more skills as needed
 ];
